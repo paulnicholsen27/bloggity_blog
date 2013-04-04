cd0f380c9dba5eb9f71346f6dd6f891b280651a6
Paul Nichols <pnichols104@gmail.com>
2013-04-04 16:17:24 -0400

    Trying again

--- hukkster/blog/templatetags/catalog_filters.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter(name='shorten')
-def shorten(content):
-	#will show up to the first 500 characters of the post content
-	return content[:500]
